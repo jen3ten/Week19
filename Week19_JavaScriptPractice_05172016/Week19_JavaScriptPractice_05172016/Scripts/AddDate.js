a83e32e942ad71057512bf08fd66a355e2b4a650
Jennifer Evans <jenevans3ten@gmail.com>
2016-06-09 17:06:00 -0400

    Commit unknown updates

---
+++ Week19_JavaScriptPractice_05172016/Week19_JavaScriptPractice_05172016/Scripts/AddDate.js
@@ -0,0 +1,48 @@
+﻿var today = new Date();
+var bday = new Date(1969, 02, 10, 00, 00, 00);
+var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+var dateString = today.toDateString();
+
+function howOldAmI() {
+var msPerYear = 31556900000;
+var years = (today.getTime() - bday.getTime())/msPerYear;
+}
+
+function whatIsToday(today) {
+    var day = dayNames[today.getDay()];
+    return day;
+}
+
+function whatIsTomorrow(today) {
+    var dayNum = today.getDay();
+    var tomorrowNum;
+    if (dayNum == 6){
+        tomorrowNum = 0;
+    }
+    else {
+        tomorrowNum = dayNum + 1;
+    }
+    var tomorrow = dayNames[tomorrowNum];
+    return tomorrow;
+    //return tomorrowNum;
+    //return dayNum;
+}
+
+
+var eltomorrow = document.getElementById('thisIsTomorrow');
+//el.innerhtml = datestring;
+//el.innerhtml = ((today.gettime() - bday.gettime())/msperyear).tofixed(2);
+//el.innerhtml = whatistoday(today);
+eltomorrow.innerHTML = whatIsTomorrow(today);
+//el.innerhtml = day;
+
+var elToday = document.getElementById('thisIsToday');
+elToday.innerHTML = whatIsToday(today);
+
+//var elTomorrow = document.getElementById('thisIsTomorrow');
+//elTomorrow.innerHTML = 'Test 1';
+
+//var elToday = document.getElementById('thisIsToday');
+//elToday.innerHTML = 'Test 2';
+
